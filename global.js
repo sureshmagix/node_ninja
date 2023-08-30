c426c0347e5c396befc97528d310f0e02440f99d
sureshmagix <sureshmagix@gmail.com>
2023-08-30 23:12:49 +0530

    [MSG:Global and intervals]

---
+++ global.js
@@ -0,0 +1,17 @@
+//console.log(global);
+
+//global.setTimeout(() => { We are not using global in the second line as its already available
+// set Time out runs a function only once
+
+
+// setTimeout(() => {
+//     console.log('In the time out');
+// }, 3000);
+
+// setInterval(()=>{
+//     console.log("This is run at every interval")
+// },1000);
+
+console.log(__dirname); // displays the current director
+console.log(__filename); // displays the file name
+
---
+++ test.js
@@ -0,0 +1,11 @@
+// const name='mario';
+
+// console.log(name);
+
+const greet = (name)=>{
+    console.log(`Hello - ${name}`);
+}
+
+
+greet('suresh');
+greet('ramesh');
