ccba1ce72171e2ddecc3e6193fc257540dfcd8ac
suresh babu <sureshmagix@gmail.com>
2023-08-31 11:12:25 +0530

    [MSG:Get os related details]

---
+++ os.js
@@ -0,0 +1,25 @@
+//https://nodejs.org/api/os.html
+const os = require('os');
+
+//console.log(os.cpus());
+
+//console.log(os.platform());
+
+//console.log(os);
+//console.log(os.hostname());
+
+console.log(os.uptime());
+
+console.log(os.homedir());
+
+console.log(os.networkInterfaces());1
+
+
+
+
+
+
+
+
+
+
