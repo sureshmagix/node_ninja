7564864dd50992b9dc879c778ac843f1c03e4b79
sureshmagix <sureshmagix@gmail.com>
2023-08-31 21:44:35 +0530

    '

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req,res)=>{
+    console.log('Request made');
+});
+
+
+server.listen(3000,'localhost',()=>{
+    console.log('listening for request on port 3000');
+})
